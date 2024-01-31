3fa14e1377648deb5524387fd686720222f551f5
NoamLandau21770 <157605691+NoamLandau21770@users.noreply.github.com>
2024-01-31 14:12:15 +0200

    touch sensor And comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -14,22 +15,35 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 // @Disabled // - Disable When Not Needed
 public class AutonomousTest extends OpMode {
 
-    private DistanceSensor distanceSensor;
-    private NormalizedColorSensor colorSensor;
+    private DistanceSensor distanceSensor;      // The Actual distance sensor
+    private NormalizedColorSensor colorSensor;  // The Actual color sensor
+    private TouchSensor touchSensor;            // The Actual touch sensor
+    private boolean touchSensorState;           // Checking if touchSensor is Pressed
     @Override
     public void init() {
         distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
         colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
+        touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
     }
 
     @Override
     public void loop() {
         telemetry.addData("Distance", String.format("%.2f", distanceSensor.getDistance(DistanceUnit.MM)));
 
-        NormalizedRGBA colors = colorSensor.getNormalizedColors();
+        NormalizedRGBA colors = colorSensor.getNormalizedColors(); // the colors of colorSensor
         telemetry.addLine("Color")
                 .addData("Red", "%.3f", colors.red)
                 .addData("Green", "%.3f", colors.green)
                 .addData("Blue", "%.3f", colors.blue);
+    
+        touchSensorState = touchSensor.isPressed();
+        if(touchSensorState)
+        {
+            telemetry.addData("Touch Sensor: Is pressed")
+        }
+        else
+        {
+            telemetry.addData("Touch Sensor: Is NOT pressed")
+        }
     }
 }
