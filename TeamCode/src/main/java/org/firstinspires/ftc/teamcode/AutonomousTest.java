e4d2e28ff79b69f5c16d992345629e384d5537b9
NoamLandau21770 <157605691+NoamLandau21770@users.noreply.github.com>
2024-02-02 09:06:03 +0200

    changing the test to work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -19,11 +20,18 @@ public class AutonomousTest extends OpMode {
     private DistanceSensor distanceSensor;      // The Actual distance sensor
     private NormalizedColorSensor colorSensor;  // The Actual color sensor
     private TouchSensor touchSensor;            // The Actual touch sensor
+    private DcMotor armAngle;                   // DcMotor for spining the claw
+    private final int i = 50;                   // number of times the armAngle spins
+
+
     @Override
     public void init() {
         distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
         colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
         touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
+        armAngle = hardwareMap.get(DcMotor.class, "armAngle");
+        armAngle.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
     }
 
     @Override
@@ -37,5 +45,11 @@ public class AutonomousTest extends OpMode {
                 .addData("Blue", "%.3f", colors.blue);
     
             telemetry.addData("Touch Sensor", touchSensor.isPressed() ? "Is pressed" : "Is NOT pressed");
+        
+        int amount = 0;
+        while(amount <= i)
+        {
+            armAngle.setPower(1);
+            amount++;
         }
 }
