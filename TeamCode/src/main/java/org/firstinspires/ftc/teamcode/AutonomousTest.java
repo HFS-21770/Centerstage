8ae4bb217616751e9006c35aec760dcd58f9720e
NoamLandau21770 <157605691+NoamLandau21770@users.noreply.github.com>
2024-02-06 20:17:43 +0200

    I put thing to help debugging and cleaned the code from unnecessary things.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -17,35 +17,24 @@ import com.sun.tools.javac.util.Convert;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @Autonomous(name = "Auto test 4.2.24")
 public class autoTest extends OpMode{
-    private DistanceSensor distanceSensor;      // The Actual distance sensor
-    private DistanceSensor distanceSensorB;
-    private NormalizedColorSensor colorSensor;  // The Actual color sensor
-    private TouchSensor touchSensor;            // The Actual touch sensor
-    private DcMotor armAngle;                   // DcMotor for spining the claw
-    private int i = 0;                   // number of times the armAngle spins
+    private DistanceSensor distanceSensor;      // The Actual distance sensor on the ____
+    private DistanceSensor distanceSensorB;     // The Actual distance sensor on the ____
+
     private DcMotor frontLeft0;
     private DcMotor frontRight3;
     private DcMotor backLeft1;
     private DcMotor backRight2;
-    private double h = 0;
-    private double r = 0;
-    private double g = 0;
-    private double b = 0;
-    private boolean timerStarted; // to reset the timer
-    private boolean walk; // if the robot walked a tile to detect the cube
-    private boolean detect; // if the robot detected the cube
-    private boolean see;
+
     public State curState;
 
+    private boolean timerStarted; // to reset the timer
     private ElapsedTime timer = new ElapsedTime();
+
     @Override
-    public void init() {
+    public void init() 
+    {
         distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
         distanceSensorB = hardwareMap.get(DistanceSensor.class, "distanceSensorB");
-        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
-        touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
-//        armAngle = hardwareMap.get(DcMotor.class, "armAngle");
-//        armAngle.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         frontLeft0 = hardwareMap.get(DcMotor.class, "frontLeft0");
         frontRight3 = hardwareMap.get(DcMotor.class, "frontRight3");
@@ -57,15 +46,12 @@ public class autoTest extends OpMode{
         backRight2.setDirection(DcMotor.Direction.FORWARD);
         frontRight3.setDirection(DcMotor.Direction.FORWARD);
 
-        telemetry.addData("Initialized", "Finished with no errors");
-
         timer.reset();
-
         timerStarted = true;
-        walk = false;
-        detect = true;
-        see = false;
+
         curState = State.DRIVE;
+
+        telemetry.addData("Initialized", "Finished with no errors");
     }
 
     @Override
@@ -75,53 +61,48 @@ public class autoTest extends OpMode{
         telemetry.addData("Distance", String.format("%.2f", distanceSensor.getDistance(DistanceUnit.MM)));
         telemetry.addData("Distance", String.format("%.2f", distanceSensorB.getDistance(DistanceUnit.MM)));
 
-
-        NormalizedRGBA colors = colorSensor.getNormalizedColors(); // the colors of colorSensor
-        telemetry.addLine("Color")
-                .addData("Red", "%.3f", colors.red)
-                .addData("Green", "%.3f", colors.green)
-                .addData("Blue", "%.3f", colors.blue);
-
-        telemetry.addData("Touch Sensor", touchSensor.isPressed() ? "Is pressed" : "Is NOT pressed");
-
         telemetry.addData("Timer2", timer.seconds());
 
-
-        switch (curState){
+        switch (curState)
+        {
             case DRIVE:
                 if(timerStarted)
                 {
                     timer.reset();
                     timerStarted = false;
                 }
-                if(timer.seconds() < 2)
+                if(timer.seconds() < 0.94)
                 {
                     frontLeft0.setPower(0.5);
                     backRight2.setPower(0.5);
                     frontRight3.setPower(0.5);
                     backLeft1.setPower(0.5);
                 }
-                curState = State.SEARCH;
+                else
+                {
+                    // 0.94 seconds passed - move to next state
+                    curState = State.SEARCH;
+                }
                 telemetry.addData("curState:",curState);
+                
                 break;
 
-
             case SEARCH:
+                telemetry.addData("Searching","...");
                 if(distanceSensor.getDistance(DistanceUnit.MM) < 300)
                 {
-                    SearchAction(State.TURNLEFT, 1);
+                    telemetry.addData("Found it on", " Right side"); // NEEDS TO BE CHANGED DEPENDING ON THE SIDES!!!
+                    SearchAction(State.TURNLEFT, 3); // the timer resets here
                 }
                 else if (distanceSensorB.getDistance(DistanceUnit.MM) < 300)
                 {
-                    SearchAction(State.TURNRIGHT, 2);
-                }
-                else if (distanceSensorB.getDistance(DistanceUnit.MM) > 300 && distanceSensor.getDistance(DistanceUnit.MM) > 300)
-                {
-                    SearchAction(State.DONTTURN, 3);
+                    telemetry.addData("Found it on", " Left side"); // NEEDS TO BE CHANGED DEPENDING ON THE SIDES!!!
+                    SearchAction(State.TURNRIGHT, 9); // the timer resets here
                 }
                 else
                 {
-                    telemetry.addData("State","ERROR!!!!!");
+                    telemetry.addData("Found it on", " upfront");
+                    SearchAction(State.DONTTURN, 12); // the timer resets here
                 }
                 break;
 
@@ -133,29 +114,39 @@ public class autoTest extends OpMode{
                     frontRight3.setPower(-0.5);
                     backLeft1.setPower(0.5);
                 }
-                curState = State.PUT;
+                else
+                {
+                    telemetry.addData("Turned Left", "Stoping robot")
+                    curState = State.PUT; 
+                }
                 break;
 
             case TURNRIGHT:
-                if(timer.seconds() < 0.5){
+                if(timer.seconds() < 0.5)
+                {
                     frontLeft0.setPower(-0.5);
                     backRight2.setPower(0.5);
                     frontRight3.setPower(0.5);
                     backLeft1.setPower(-0.5);
                 }
-                else{
+                else
+                {
+                    telemetry.addData("Turned Right", "Stoping robot")
                     curState = State.PUT;
 
                 }
                 break;
             case DONTTURN:
-                if(timer.seconds() < 0.5){
-                    frontLeft0.setPower(0.5);
-                    backRight2.setPower(0.5);
-                    frontRight3.setPower(0.5);
-                    backLeft1.setPower(0.5);
+                if(timer.seconds() < 0.5)
+                {
+                    frontLeft0.setPower(-0.5);
+                    backRight2.setPower(-0.5);
+                    frontRight3.setPower(-0.5);
+                    backLeft1.setPower(-0.5);
                 }
-                else {
+                else 
+                {
+                    telemetry.addData("Moved Backwords", "Stoping robot");
                     curState = State.PUT;
                 }
                 break;
@@ -164,47 +155,21 @@ public class autoTest extends OpMode{
                 backRight2.setPower(0);
                 frontRight3.setPower(0);
                 backLeft1.setPower(0);
+                break;
+
+            default
+            {
+                telemetry.addData("State not valid, somthing went wrong", String.valueOf(curState));
+            }
         }
-
-
-//        if(!detect && distanceSensor.getDistance(DistanceUnit.MM) < 300)
-//        {
-//            telemetry.addData("Cube:","1");
-//            frontLeft0.setPower(0.5);
-//            backRight2.setPower(-0.5);
-//            frontRight3.setPower(-0.5);
-//            backLeft1.setPower(0.5);
-//            detect = true;
-//            see = true;
-//        }
-//        else if(!detect && distanceSensorB.getDistance(DistanceUnit.MM) < 300)
-//        {
-//            telemetry.addData("Cube:","2");
-//            frontLeft0.setPower(-0.5);
-//            backRight2.setPower(0.5);
-//            frontRight3.setPower(0.5);
-//            backLeft1.setPower(-0.5);
-//            see = true;
-//            detect = true;
-//
-//        }
-//        else if(!detect && distanceSensorB.getDistance(DistanceUnit.MM) > 300 && distanceSensor.getDistance(DistanceUnit.MM) > 300)
-//        {
-//            telemetry.addData("Cube:","3");
-//            frontLeft0.setPower(0);
-//            backRight2.setPower(0);
-//            frontRight3.setPower(0);
-//            backLeft1.setPower(0);
-//            see = true;
-//            detect = true;
-//        }
+        
     }
     public enum State {
         DRIVE, SEARCH, TURNLEFT,TURNRIGHT,DONTTURN, PUT
     }
 
     public void SearchAction(State state, int value) {
-        telemetry.addData("Cube:", String.valueOf(value));
+        telemetry.addData("Found In:", String.valueOf(value));
         curState = state;
         timer.reset();
     }
