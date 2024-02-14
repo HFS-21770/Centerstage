798ea7d6d768188b139fecb1bef8cc45098c6657
namd-frc <157491694+namd-ftc@users.noreply.github.com>
2024-02-14 13:13:45 +0200

    Update Manual

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualLinear.java
@@ -1,18 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
 
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.vision.VisionPortal;
 
-@TeleOp(name = "Manual")
+@TeleOp(name = "ManualLinear")
 // @Disabled
-public class Manual extends OpMode {
+public class ManualLinear extends LinearOpMode {
 
     private ServoController servoController;
     private DcMotor arm1;
@@ -25,8 +23,21 @@ public class Manual extends OpMode {
     private DcMotor backLeft1;
     private DcMotor backRight2;
     private DcMotor suspension;
+
     @Override
-    public void init() {
+    public void runOpMode() {
+        initRobot();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            loopCode();
+        }
+
+        stopRobot();
+    }
+
+    public void initRobot() {
         // Get Control Hub ServoController
         servoController = hardwareMap.get(ServoController.class, "Control Hub");
 
@@ -82,51 +93,64 @@ public class Manual extends OpMode {
         telemetry.addData("Initialized", "Finished with no errors");
     }
 
-    @Override
-    public void loop() {
-        // Reverse stick value in order to flip forward and backward movements to be correct;
-        double forward = -gamepad1.left_stick_y;
 
-        // Factor to counteract imperfect strafing;
-        double turn = gamepad1.left_stick_x * 1.1;
-        double strafe = gamepad1.right_stick_x;
+    public void loopCode() {
+        // Get Control Hub ServoController
+        servoController = hardwareMap.get(ServoController.class, "Control Hub");
 
-        // Arm up/down speed;
-        double arm = gamepad2.left_stick_y * 0.5;
+        // Get Arm Motors
+        arm1 = hardwareMap.get(DcMotor.class, "arm1");
+        arm2 = hardwareMap.get(DcMotor.class, "arm2");
+        DcMotor armSpin = hardwareMap.get(DcMotor.class, "armSpin");
 
-        // Power for Arm Motors (-1 to 1);
-        motorPower(forward, strafe, turn);
+        // Get Suspension Motor
+        suspension = hardwareMap.get(DcMotor.class, "suspension");
 
-        // Change arm Lift speed;
-        arm1.setPower(arm);
-        arm2.setPower(arm);
+        // Get servos
+        claw = hardwareMap.get(Servo.class, "claw");
+        angle = hardwareMap.get(Servo.class, "angle");
 
-        // Control Claws
-        claw.setPosition(gamepad2.left_trigger / 2);
+        // Plane launcher
+        plane = hardwareMap.get(Servo.class, "plane");
 
-        // Claw Angle
-        if (gamepad2.dpad_down) {
-            angle.setPosition(angle.getPosition() + 0.1);
-        } else if (gamepad2.dpad_up) {
-            angle.setPosition(angle.getPosition() - 0.1);
-        }
+        // Get Wheel Motors
+        frontLeft0 = hardwareMap.get(DcMotor.class, "frontLeft0");
+        frontRight3 = hardwareMap.get(DcMotor.class, "frontRight3");
+        backLeft1 = hardwareMap.get(DcMotor.class, "backLeft1");
+        backRight2 = hardwareMap.get(DcMotor.class, "backRight2");
 
-        if (gamepad2.a) {
-            suspension.setPower(1);
-        } else if (gamepad2.y) {
-            suspension.setPower(-1);
-        } else {
-            suspension.setPower(0);
-        }
+        // Enable Control Hub Servos;
+        servoController.pwmEnable();
 
-        // Launch plane
-        if (gamepad1.x) {
-            plane.setPosition(0);
-        }
+        // Configure Max Angles for the servos that need it;
+        claw.scaleRange(0, 0.5);
+        angle.scaleRange(0, 0.55);
+
+        // Set Servos to starting positions;
+        claw.setPosition(0);
+        plane.setPosition(0.1);
+
+        // Configure DcMotor Directions correctly;
+        // Wheels;
+        frontLeft0.setDirection(DcMotor.Direction.REVERSE);
+        backLeft1.setDirection(DcMotor.Direction.REVERSE);
+        backRight2.setDirection(DcMotor.Direction.FORWARD);
+        frontRight3.setDirection(DcMotor.Direction.FORWARD);
+        // Arm;
+        arm1.setDirection(DcMotor.Direction.FORWARD);
+        arm2.setDirection(DcMotor.Direction.REVERSE);
+        suspension.setDirection(DcMotor.Direction.REVERSE);
+
+        // configure Arm brakes; to counteract Arm not being able to hold itself;
+        arm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armSpin.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Update on Driver Hub that Initialization finished;
+        telemetry.addData("Initialized", "Finished with no errors");
     }
 
-    @Override
-    public void stop() {
+    public void stopRobot() {
         // Prepare Plane servo for reloading;
         plane.setPosition(1);
     }
