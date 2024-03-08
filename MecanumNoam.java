f050bb5f23ed822ad0b8e5b0c35416e53d742034
NoamLandau21770 <157605691+NoamLandau21770@users.noreply.github.com>
2024-03-08 09:16:26 +0200

    Disabled

--- MecanumNoam.java
+++ MecanumNoam.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,6 +10,7 @@ import com.qualcomm.robotcore.hardware.ServoController;
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
 @TeleOp(name = "Mecanum Shlomi")
+@Disabled
 public class MecanumNoam extends OpMode{
 
     private ServoController ServoController;
